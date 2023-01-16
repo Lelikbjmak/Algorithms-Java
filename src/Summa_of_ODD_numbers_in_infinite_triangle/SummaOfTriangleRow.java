c65fe836e210d28603297b048bc9f2439cbded9e
Denis Solovey <denissolovey990@gmail.com>
2023-01-16 19:30:22 +0300

    Summa of odd numbers in odd pyramid

---
+++ src/Summa_of_ODD_numbers_in_infinite_triangle/SummaOfTriangleRow.java
@@ -0,0 +1,17 @@
+package Summa_of_ODD_numbers_in_infinite_triangle;
+
+public class SummaOfTriangleRow {
+
+    // Enter the number of row -> must obtain summa of all elements in a row
+    //            1
+    //          3   5
+    //        7   9   11
+    // example of such triangle : input 1 -> 1; input 2 -> 3+5 = 8 etc...
+    //input 2 -> 3+5 = 8 (2^3)  input 3 -> 7+9+11=27 (3^3)  input 4 -> 13+15+17+19 = 64 (4^3)
+
+
+
+    public static int summaOfRawNumber(int rawNumber){
+        return rawNumber * rawNumber * rawNumber;
+    }
+}
---
+++ src/Summa_of_ODD_numbers_in_infinite_triangle/SummaOfTriangleRowTest.java
@@ -0,0 +1,14 @@
+package Summa_of_ODD_numbers_in_infinite_triangle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SummaOfTriangleRowTest {
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(1, SummaOfTriangleRow.summaOfRawNumber(1));
+        Assertions.assertEquals(74088, SummaOfTriangleRow.summaOfRawNumber(42));
+    }
+
+}
